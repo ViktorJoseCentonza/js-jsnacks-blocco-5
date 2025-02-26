533279ddf6751df5aaf9fb8e0e2fc61f03221b29
ViktorJoseCentonza <viktorjose02@gmail.com>
2025-02-26 14:44:58 +0100

    add snack 5

--- ex-js-array-methods-main/snack-5/script.js
+++ ex-js-array-methods-main/snack-5/script.js
@@ -2,3 +2,15 @@ const nums = [2, 8, 4, 7, 12, 87];
 
 // Crea un array a partire dall'array nums, che abbia solo i numeri pari
 // Risultato: [2, 8, 4, 12]
+
+/*
+//works but leaves undefined where it doesn't find an even number
+const evenNums = nums.map((nums) => {
+    if(nums % 2 == 0){
+        return nums
+    }
+})
+*/
+
+const evenNums = nums.filter((nums) => nums % 2 == 0)
+console.log(evenNums);
